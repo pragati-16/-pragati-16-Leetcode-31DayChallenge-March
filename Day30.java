8ef381d6c0279368c22a30cb2aaaf0d90e2b7ef6
Pragati Gupta <73828380+pragati-16@users.noreply.github.com>
2022-03-30 22:43:15 +0530

    Add files via upload

---
+++ Day30.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean search(int matrix[][],int n,int m,int a,int b,int target) {
+     if(a==m&&b==n)
+         return false;
+        int low=b,end=n-1;
+        while(low<=end) {
+        int mid=low+(end-low)/2;
+         if(matrix[a][mid]==target)
+        return true;
+        if(matrix[a][mid]>target)
+        end=mid-1;
+        else
+        low=mid+1;
+        }
+        return false;
+    }
+    public boolean searchMatrix(int[][] matrix, int target) {
+      int m=matrix.length;
+        int n=matrix[0].length;
+        int j=0;
+        for(int i=0;i<m;i++) {
+           if(target>=matrix[i][j]&&target<=matrix[i][n-1]) {
+             return search(matrix,n,m,i,j,target);
+           }
+        }
+        return false;
+    }
+}
